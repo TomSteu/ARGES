5346b07ec408c535a07ebd247a68ad965767c2e0
tst <steudtner.tom@gmail.com>
2017-01-17 19:18:19 +0000

    symmetrize quartics, some changes to enhance perfomance with quartic^2 term

--- ARGES.m
+++ ARGES.m
@@ -4,6 +4,7 @@ BeginPackage["ARGES`"];
 	RealScalar::usage = "Add real scalar by RealScalar[symbol, Flavors, {Representation1, ...}]";
 	ComplexScalar::usage = "Add complex scalar by ComplexScalar[Symbol, Flavors, {Representation1, ...}]; this adds the real scalars Re[Symbol] and Im[Symbol]";
 	YukawaYaij::usage = "Add Yukawa matrix term (with h.c.) YukawaYaij[Symbol, ScalarField, LeftFermion, RightFermion, {List of contractions for each gauge}]; Contractions are functions [ScalarIndex, LeftFermionIndex, RightFermionIndex], example: Yaijk[y, S, F1, F2, ...] == - y adj[F1].S.F2 + h.c.";
+	Quartic\[Lambda]abcd::usage = "Add scalar quartic coupling Quartic\[Lambda]abcd[Symbol, Scalar1, Scalar2, Scalar3, Scalar4, {List of contractions for each gauge}, prefactor and contraction of flavors], complex scalars will be rewritten as real and imaginary part and the quartic is symmetrized totally automatically. No additional 1/4! will be added to the prefactor, but there might be a factor to keep the norm after symmetrization";
 	\[Beta]::usage = "Display Beta function for gauge coupling \[Beta][Gauge, LoopLevel];";
 	Reset::usage = "reset/initiate package";
 	ComputeInvariants::usage = "Calculates known invariants for beta functions, saves them as rules in subInvariants";
@@ -34,6 +35,7 @@ BeginPackage["ARGES`"];
 			ListGauge = {};
 			ListYukawa = {};
 			ListQuartic = {};
+			ListQuarticSym = {};
 			WeylFermionList = {};
 			RealScalarList = {};
 			ComplexScalarList = {};
@@ -125,7 +127,7 @@ BeginPackage["ARGES`"];
 		];
 		
 		Quartic\[Lambda]abcd[sym_, Sa_, Sb_, Sc_, Sd_, gauge_List, fak_:(1&)] := Module[
-			{posA, posB, posC, posD},
+			{posA, posB, posC, posD, permList1, permList2, fakHold, xHold},
 			posA = Position[ComplexScalarList, Sa];
 			If[posA != {},
 				Quartic\[Lambda]abcd[sym, Re[Sa], Sb, Sc, Sd, gauge, (1/Sqrt[2] fak[#1,#2,#3,#4])&];
@@ -183,7 +185,31 @@ BeginPackage["ARGES`"];
 				If[Dimensions[gauge][[1]] != NumberOfSubgroups,
 					Message[Quartic::ContractionError];,
 					ListQuartic = Append[ListQuartic, {sym, posA[[1,1]], posB[[1,1]], posC[[1,1]], posD[[1,1]], gauge, fak}];
+					permList1 = PermList[List[posA[[1,1]], posB[[1,1]], posC[[1,1]], posD[[1,1]]]];
+					permList2 = PermList[List["a","b","c","d"]];
+					For[ii=1, ii<= 24, ii++, 
+						AppendSymQuartic[
+							sym, permList1[[ii,1]], permList1[[ii,2]], permList1[[ii,3]], permList1[[ii,4]], 
+							Function[{x}, Evaluate[PermList[xHold[#1,#2,#3,#4]][[ii]]]&/.xHold[a_,b_,c_,d_]->x[a,b,c,d]]/@gauge,
+							Evaluate[PermList[1/24 fakHold[#1,#2,#3,#4]][[ii]]]&/.fakHold[a_,b_,c_,d_]->fak[a,b,c,d]
+						];
+					];
 				];
+			];
+		];
+		
+		AppendSymQuartic[sym_, pa_, pb_, pc_, pd_, gauge_, fak_] := Module[
+			{pos, ii, dum1, dum2, dum3, dum4},
+			pos = 0;
+			For[ii = 1, ii <= Dimensions[ListQuarticSym][[1]], ii++,
+				If[
+					ListQuarticSym[[ii,1]] == sym && ListQuarticSym[[ii,2]] == pa && ListQuarticSym[[ii,3]] == pb && ListQuarticSym[[ii,4]] == pc && ListQuarticSym[[ii,5]] == pd && And@@(Function[{x}, ListQuarticSym[[ii,6,x]][dum1, dum2, dum3, dum4] === gauge[[x]][dum1, dum2, dum3, dum4]]/@Range[NumberOfSubgroups]),
+					pos = ii;
+				];
+			];
+			If[pos == 0,
+				ListQuarticSym = Append[ListQuarticSym, {sym, pa, pb, pc, pd, gauge, fak}];,
+				ListQuarticSym[[pos, 7]] = Evaluate[ListQuarticSym[[pos, 7]][#1,#2,#3,#4] + fak[#1,#2,#3,#4]]&;
 			];
 		];
 		
@@ -245,8 +271,8 @@ BeginPackage["ARGES`"];
 			pc = Join[{c}, lc];
 			pd = Join[{d}, ld];
 			Return[
-				ReleaseHold[(Quartic[pa[[1]],pb[[1]],pc[[1]],pd[[1]]]/.subQuart)//.{
-					Quart[q_]:>((ListQuartic[[q,1]] ListQuartic[[q,7]][pa[[2]], pb[[2]], pc[[2]], pd[[2]]])(Times@@(Function[{x},ListQuartic[[q,6,x]][pa[[2+x]], pb[[2+x]], pc[[2+x]], pd[[2+x]]]]/@Range[NumberOfSubgroups])))}
+				ReleaseHold[(SymQuartic[pa[[1]],pb[[1]],pc[[1]],pd[[1]]]/.subQuart)//.{
+					SymQuart[q_]:>((ListQuarticSym[[q,1]] ListQuarticSym[[q,7]][pa[[2]], pb[[2]], pc[[2]], pd[[2]]])(Times@@(Function[{x},ListQuarticSym[[q,6,x]][pa[[2+x]], pb[[2+x]], pc[[2+x]], pd[[2+x]]]]/@Range[NumberOfSubgroups])))}
 				]
 			];
 		];
@@ -261,7 +287,6 @@ BeginPackage["ARGES`"];
 			beta = beta + 3/4 Sum[Sqr[ListGauge[[ii,1]]] Sqr[ListGauge[[ii2,1]]] Perm[As[ii,ii2][Join[{pa}, la], Join[{pb}, lb], Join[{pc}, lc], Join[{pd}, ld]]], {ii, 1, NumberOfSubgroups}, {ii2, 1, NumberOfSubgroups}]//.subQuartInv;
 			Return[beta/(24 Sqr[4\[Pi]])];
 		]
-		
 		
 		
 		(* Definition of Invariants *)
@@ -465,30 +490,41 @@ BeginPackage["ARGES`"];
 			Quart[q_] :> Join[
 				{{ListQuartic[[q,1]], ListQuartic[[q,7]], RealScalarList[[ListQuartic[[q,2]], 2]], RealScalarList[[ListQuartic[[q,3]], 2]], RealScalarList[[ListQuartic[[q,4]], 2]], RealScalarList[[ListQuartic[[q,5]], 2]]}}, 
 				Function[{x}, If[ListGauge[[x,3]] === 1, {ListQuartic[[q,6,x]], 1, 1, 1, 1}, {ListQuartic[[q,6,x]], RealScalarList[[ListQuartic[[q,2]], 3, x]], RealScalarList[[ListQuartic[[q,3]], 3, x]], RealScalarList[[ListQuartic[[q,4]], 3, x]], RealScalarList[[ListQuartic[[q,5]], 3, x]]}]]/@Range[NumberOfSubgroups]
+			],
+			SymQuartic[a_, b_, c_, d_] :> Block[
+				{pos, qq},
+				pos = {};
+				If[ListQuartic != {},
+					For[qq=1, qq<=Dimensions[ListQuarticSym][[1]], qq++,
+						If[ListQuarticSym[[qq,2]] == a && ListQuarticSym[[qq,3]] == b && ListQuarticSym[[qq, 4]] == c && ListQuarticSym[[qq,5]] == d,
+							pos = Append[pos, qq];
+						];
+					];
+				];
+				If[pos == {}, 
+					0,
+					Plus@@(Hold/@(SymQuart/@pos))
+				]
+			],
+			SymQuart[q_] :> Join[
+				{{ListQuarticSym[[q,1]], ListQuarticSym[[q,7]], RealScalarList[[ListQuarticSym[[q,2]], 2]], RealScalarList[[ListQuarticSym[[q,3]], 2]], RealScalarList[[ListQuarticSym[[q,4]], 2]], RealScalarList[[ListQuarticSym[[q,5]], 2]]}}, 
+				Function[{x}, If[ListGauge[[x,3]] === 1, {ListQuarticSym[[q,6,x]], 1, 1, 1, 1}, {ListQuarticSym[[q,6,x]], RealScalarList[[ListQuarticSym[[q,2]], 3, x]], RealScalarList[[ListQuarticSym[[q,3]], 3, x]], RealScalarList[[ListQuarticSym[[q,4]], 3, x]], RealScalarList[[ListQuarticSym[[q,5]], 3, x]]}]]/@Range[NumberOfSubgroups]
 			]
 		};
 		
 		subQuartInv := {
 			\[CapitalLambda][pa_, pb_, pc_, pd_] :> Block[
-				{sum, i1, i2, s1, s2, perm1, perm2},
+				{},
 				sum=0;
-				For[s1[0]=0, s1[0]<=SNumber[], s1[0]++,
-					For[s2[0]=0, s2[0]<=SNumber[], s2[0]++,
-						perm1 = Permutations[{pa, pb, Function[{x}, s1[x]]/@Range[0,NumberOfSubgroups+1], Function[{x}, s2[x]]/@Range[0,NumberOfSubgroups+1]}];
-						perm2 = Permutations[{Function[{x}, s1[x]]/@Range[0,NumberOfSubgroups+1], Function[{x}, s2[x]]/@Range[0,NumberOfSubgroups+1], pc, pd}];
-						For[i1=1, i1<=Dimensions[perm1][[1]], i1++,
-							For[i2=1, i2<=Dimensions[perm1][[1]], i2++,
-								sum = sum + (
-									ReleaseHold[
-										prod[
-											Quartic[perm1[[i1, 1]][[1]], perm1[[i1, 2]][[1]], perm1[[i1, 3]][[1]], perm1[[i1, 4]][[1]]], 
-											Quartic[perm2[[i2, 1]][[1]], perm2[[i2, 2]][[1]], perm2[[i2, 3]][[1]], perm2[[i2, 4]][[1]]]
-										]//.subProd/.subQuart//.subProd]//.subQuart/.{prod[A_List, B_List]:>(SolveSProd2[A, B, Function[{x},
-											((KroneckerDelta[perm1[[i1,1]][[1+x]],#1] KroneckerDelta[perm1[[i1,2]][[1+x]],#2] KroneckerDelta[perm1[[i1,3]][[1+x]],#3] KroneckerDelta[perm1[[i1,4]][[1+x]],#4] KroneckerDelta[perm2[[i2,1]][[1+x]],#5] KroneckerDelta[perm2[[i2,2]][[1+x]],#6] KroneckerDelta[perm2[[i2,3]][[1+x]],#7] KroneckerDelta[perm2[[i2,4]][[1+x]],#8])//.{KroneckerDelta[s1[x], aa_] KroneckerDelta[s1[x], bb_] :> KroneckerDelta[aa,bb], KroneckerDelta[s2[x], aa_] KroneckerDelta[s2[x], bb_] :> KroneckerDelta[aa,bb], Power[KroneckerDelta[s1[x], aa_],2] :> KroneckerDelta[aa,aa], Power[KroneckerDelta[s2[x], aa_],2] :> KroneckerDelta[aa,aa]})&
-									]/@Range[NumberOfSubgroups+1]])}
-								)/(Dimensions[perm1][[1]] Dimensions[perm2][[1]])
-							];
-						];
+				For[s1=1, s1<=SNumber[], s1++,
+					For[s2=1, s2<=SNumber[], s2++,
+						sum += (ReleaseHold[
+								prod[
+									SymQuartic[pa[[1]], pb[[1]], s1, s2], 
+									SymQuartic[s1, s2, pc[[1]], pd[[1]]]
+								]//.subProd/.subQuart//.subProd
+							]//.subQuart/.{prod[A_List, B_List]:>(SolveSProd2[A, B, Function[{x}, (KroneckerDelta[pa[[x+1]], #1] KroneckerDelta[pb[[x+1]], #2] KroneckerDelta[#3, #5] KroneckerDelta[#4, #6] KroneckerDelta[pc[[x+1]], #7] KroneckerDelta[pd[[x+1]], #8])&]/@Range[NumberOfSubgroups+1]])}
+						);
 					];
 				];
 				sum
@@ -497,27 +533,66 @@ BeginPackage["ARGES`"];
 				{x},
 				(KroneckerDelta[#1,pa[[1+x]]] KroneckerDelta[#2,pb[[1+x]]] KroneckerDelta[#3,pc[[1+x]]] KroneckerDelta[#4,pd[[1+x]]])&
 			]/@Range[NumberOfSubgroups+1]],
-			\[CapitalLambda]Y[pa_, pb_, pc_, pd_] :> ReleaseHold[(ReleaseHold[(Quartic[pa[[1]],pb[[1]],pc[[1]],pd[[1]]]/.subQuart)//.{
-					Quart[q_]:>((ListQuartic[[q,1]] ListQuartic[[q,7]][pa[[2]], pb[[2]], pc[[2]], pd[[2]]])(Times@@(Function[{x},ListQuartic[[q,6,x]][pa[[2+x]], pb[[2+x]], pc[[2+x]], pd[[2+x]]]]/@Range[NumberOfSubgroups])))
-				}]) Hold[
+			\[CapitalLambda]Y[pa_, pb_, pc_, pd_] :> ReleaseHold[
+				BetaQuartic[pa[[1]], pb[[1]], pc[[1]], pd[[1]], pa[[2;;]], pb[[2;;]], pc[[2;;]], pd[[2;;]], 0] Hold[
 					Sum[
 						YukawaTrace[Yuk[scal], adj[Yuk[scal]], Table[KroneckerDelta[#1, #2]&, {ii, NumberOfSubgroups+1}]],
 						{scal, 1, SNumber[]}
 					]
-				]],
-			\[CapitalLambda]S[gaug_][pa_, pb_, pc_, pd_] :> ReleaseHold[(ReleaseHold[(Quartic[pa[[1]],pb[[1]],pc[[1]],pd[[1]]]/.subQuart)//.{
-					Quart[q_]:>((ListQuartic[[q,1]] ListQuartic[[q,7]][pa[[2]], pb[[2]], pc[[2]], pd[[2]]])(Times@@(Function[{x},ListQuartic[[q,6,x]][pa[[2+x]], pb[[2+x]], pc[[2+x]], pd[[2+x]]]]/@Range[NumberOfSubgroups])))
-				}]) Hold[
+				]
+			],
+			\[CapitalLambda]S[gaug_][pa_, pb_, pc_, pd_] :> ReleaseHold[
+				BetaQuartic[pa[[1]], pb[[1]], pc[[1]], pd[[1]], pa[[2;;]], pb[[2;;]], pc[[2;;]], pd[[2;;]], 0] Hold[
 					Sum[
 						C2[RealScalarList[[scal,1]], ListGauge[[gaug,1]]] SMultiplicity[scal],
 						{scal, 1, SNumber[]}
 					]
-				]],
-			As[gaug1_, gaug2_][pa_, pb_, pc_, pd_] :> (
-				A[gaug1, gaug2][pa, pb, pc, pd]
-			)
+				]
+			],
+			As[gaug1_, gaug2_][a_, b_, c_, d_] :> Block[
+				{ind1, ind2, ind1L, ind2L, sum, x},
+				sum = 0;
+				ind1L = ind1/@Range[0,NumberOfSubgroups+1];
+				ind2L = ind2/@Range[0,NumberOfSubgroups+1];
+				For[ind1[0]=1, ind1[0]<=SNumber[], ind1[0]++,
+					For[ind2[0]=1, ind2[0]<=SNumber[], ind2[0]++,
+						sum += Sum@@Join[
+							{\[CapitalLambda][gaug1][a, c, ind1L, ind2L] \[CapitalLambda][gaug2][ind1L, ind2L, b, d] + \[CapitalLambda][gaug1][a, ind1L, ind2L, d] \[CapitalLambda][gaug2][ind1L, b, c, ind2L],
+							{ind1[1], 1, RealScalarList[[ind1[0], 2]]},
+							{ind2[1], 1, RealScalarList[[ind1[0], 2]]}},
+							Function[{x}, {ind1[x+1], 1, If[ListGauge[[x,3]]===1, 1, RealScalarList[[ind1[0], 3, x]]]}]/@Range[NumberOfSubgroups],
+							Function[{x}, {ind2[x+1], 1, If[ListGauge[[x,3]]===1, 1, RealScalarList[[ind2[0], 3, x]]]}]/@Range[NumberOfSubgroups]
+						]//.sub\[CapitalLambda];
+					];
+				];
+				sum
+			]
 		};
-
+		
+		sub\[CapitalLambda] := {
+			(* Contraction of two scalar generators, see for instance arXiv:hep-ph/0211440 eq. (117) *)
+			(** At least one is gauge singlet *)
+			\[CapitalLambda][gaug_][a_, b_, c_, d_] :> (0)/;(ListGauge[[gaug,3]] =!= 1 && (RealScalarList[[a[[1]],3,gaug]] == 1 || RealScalarList[[b[[1]],3,gaug]] == 1 || RealScalarList[[c[[1]],3,gaug]] == 1 || RealScalarList[[d[[1]],3,gaug]] == 1)),
+			(** SU(N) -- all in fundamental representation *)
+			\[CapitalLambda][gaug_][a_, b_, c_, d_] :> If[
+				((MatchQ[RealScalarList[[a[[1]], 1]], Re[_]] && MatchQ[RealScalarList[[b[[1]], 1]], Re[_]]) || 
+				(MatchQ[RealScalarList[[a[[1]], 1]], Im[_]] && MatchQ[RealScalarList[[b[[1]], 1]], Im[_]])) &&
+				((MatchQ[RealScalarList[[d[[1]], 1]], Re[_]] && MatchQ[RealScalarList[[c[[1]], 1]], Re[_]]) || 
+				(MatchQ[RealScalarList[[d[[1]], 1]], Im[_]] && MatchQ[RealScalarList[[c[[1]], 1]], Im[_]]))
+				,
+				1/2(TensorDelta[a,d] TensorDelta[b,c] - 1/ListGauge[[gaug,3]] TensorDelta[a,c] TensorDelta[b,d]),
+				0
+			]/;(
+				ListGauge[[gaug,2]] === SU && 
+				RealScalarList[[a[[1]], 3, gaug]] == ListGauge[[gaug,3]] && 
+				RealScalarList[[b[[1]], 3, gaug]] == ListGauge[[gaug,3]] && 
+				RealScalarList[[c[[1]], 3, gaug]] == ListGauge[[gaug,3]] && 
+				RealScalarList[[d[[1]], 3, gaug]] == ListGauge[[gaug,3]]
+			),
+			
+			(** U(1) *)
+			\[CapitalLambda][gaug_][a_, b_, c_, d_] :>(RealScalarList[[a[[1]],3,gaug]] RealScalarList[[b[[1]],3,gaug]] TensorDelta[a,c] TensorDelta[b,d])/;(ListGauge[[gaug, 3]] === 1)
+		};
 		(* sum over all fermions / scalars *)
 		FSum[a___] := ReleaseHold[If[WeylFermionList == {}, 0, Sum[(a)&[sumindex], {sumindex,1,FNumber[]}]]];
 		SSum[a___] := ReleaseHold[If[RealScalarList == {}, 0, Sum[(a)&[sumindex], {sumindex,1,SNumber[]}]]];
@@ -718,7 +793,9 @@ BeginPackage["ARGES`"];
 		YukawaProd[a___, pl_, pr_, LstL_, LstR_, SGauge_] := ReleaseHold[(ReleaseHold[SolveProd[a, pl, pr] //.subProd /.subYuk //.subProd]//.subYuk //.{tr[y1_, y2_]->Hold[Times@@SolveTrace2[y1, y2, SGauge]], tr[y1_, y2_, y3_]->Hold[Times@@SolveTrace3[y1, y2, y3, SGauge]], tr[y1_, y2_, y3_, y4_]->Hold[Times@@SolveTrace4[y1, y2, y3, y4, SGauge]], prod[y1_]->Hold[Times@@SolveProd1[y1, LstL, LstR, SGauge]], prod[y1_, y2_]->Hold[Times@@SolveProd2[y1, y2, LstL, LstR, SGauge]], prod[y1_, y2_, y3_]->Hold[Times@@SolveProd3[y1, y2, y3, LstL, LstR, SGauge]], prod[y1_, y2_, y3_, y4_]->Hold[Times@@SolveProd4[y1, y2, y3, y4, LstL, LstR, SGauge]], prod[y1_, y2_, y3_, y4_, y5_]->Hold[Times@@SolveProd5[y1, y2, y3, y4, y5, LstL, LstR, SGauge]]})];
 		
 		(* permutation *)
-		Perm[f_[a_,b_,c_,d_]]:=(f[a,b,c,d] + f[a,b,d,c] + f[a,c,b,d] + f[a,c,d,b] + f[a,d,b,c] + f[a,d,c,b] + f[b,a,c,d] + f[b,a,d,c] + f[b,c,a,d] + f[b,c,d,a] + f[b,d,a,c] + f[b,d,c,a] + f[c,a,b,d] + f[c,a,d,b] + f[c,b,a,d] + f[c,b,d,a] + f[c,d,a,b] + f[c,d,b,a] + f[d,a,b,c] + f[d,a,c,b] + f[d,b,a,c] + f[d,b,c,a] + f[d,c,a,b] + f[d,c,b,a]); 
+		Perm[f_[a_,b_,c_,d_]]:= f[a,b,c,d] + f[a,b,d,c] + f[a,c,b,d] + f[a,c,d,b] + f[a,d,b,c] + f[a,d,c,b] + f[b,a,c,d] + f[b,a,d,c] + f[b,c,a,d] + f[b,c,d,a] + f[b,d,a,c] + f[b,d,c,a] + f[c,a,b,d] + f[c,a,d,b] + f[c,b,a,d] + f[c,b,d,a] + f[c,d,a,b] + f[c,d,b,a] + f[d,a,b,c] + f[d,a,c,b] + f[d,b,a,c] + f[d,b,c,a] + f[d,c,a,b] + f[d,c,b,a];
+		PermList[f_[a_,b_,c_,d_]]:={f[a,b,c,d], f[a,b,d,c], f[a,c,b,d], f[a,c,d,b], f[a,d,b,c], f[a,d,c,b], f[b,a,c,d], f[b,a,d,c], f[b,c,a,d], f[b,c,d,a], f[b,d,a,c], f[b,d,c,a], f[c,a,b,d], f[c,a,d,b], f[c,b,a,d], f[c,b,d,a], f[c,d,a,b], f[c,d,b,a], f[d,a,b,c], f[d,a,c,b], f[d,b,a,c], f[d,b,c,a], f[d,c,a,b], f[d,c,b,a]};
+		PermList[s_ f_[a_,b_,c_,d_]]:={s f[a,b,c,d], s f[a,b,d,c], s f[a,c,b,d], s f[a,c,d,b], s f[a,d,b,c], s f[a,d,c,b], s f[b,a,c,d], s f[b,a,d,c], s f[b,c,a,d], s f[b,c,d,a], s f[b,d,a,c], s f[b,d,c,a], s f[c,a,b,d], s f[c,a,d,b], s f[c,b,a,d], s f[c,b,d,a], s f[c,d,a,b], s f[c,d,b,a], s f[d,a,b,c], s f[d,a,c,b], s f[d,b,a,c], s f[d,b,c,a], s f[d,c,a,b], s f[d,c,b,a]};
 		
 		(* scalar quartic products *)
 		SolveSProd2[Q1_, Q2_, SContr_] := Times@@Join[
