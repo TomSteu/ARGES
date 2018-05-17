3a55de64ccb8c9df4caf031ee342854ee80cba8a
tst <steudtner.tom@gmail.com>
2018-05-17 11:35:11 +0100

    use Dispatch on subSum to improve performance

--- ARGES.m
+++ ARGES.m
@@ -4123,7 +4123,7 @@ BeginPackage["ARGES`"];
 			SimplifySum[Generator[A___][a_, i_, j_] Generator[A___][b_, j_, i_], SS1___, {i_, 1, ii_}, SS2___, {j_, 1, jj_}, SS3___] :> SimplifySum[T2[A] KroneckerDelta[a, b], SS1, SS2, SS3],
 			SimplifySum[A_] :> A,
 			SimplifySum[] :> 0
-		};
+		} // Dispatch;
 		
 		(* like subSum, but with more advanced simplifications to be utilized in SimplifyProduct *)
 		subSum2 := {
